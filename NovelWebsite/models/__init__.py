e26fac1c24ca39bc865406b3fe38934eeee3f69e
Karloar <879865529@qq.com>
2019-02-24 19:33:06 +0800

    build the structure of novelspider

--- NovelWebsite/models/__init__.py
+++ NovelWebsite/models/__init__.py
@@ -9,13 +9,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
     password = db.Column(db.String(30))
+    email = db.Column(db.String(100))
 
 
 class UserCollection(db.Model):
     __tablename__ = 'user_collection'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
+    user = db.relationship("User", backref="user_collections")
+    novel_title_id = db.Column(db.Integer, db.ForeignKey("novel_title.id"))
+    novel_title = db.relationship("NovelTitle", backref="user_collections")
 
 
 class NovelTitle(db.Model):
@@ -23,12 +26,15 @@ class NovelTitle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     author = db.Column(db.String(50))
+    cover = db.Column(db.String(100))
+    time = db.Column(db.DateTime)
 
 
 class NovelSection(db.Model):
     __tablename__ = 'novel_section'
     id = db.Column(db.Integer, primary_key=True)
     novel_id = db.Column(db.Integer, db.ForeignKey("novel_title.id"))
+    novel_title = db.relationship("NovelTitle", backref="novel_sections")
     title = db.Column(db.String(200))
     content = db.Column(db.Text)
     time = db.Column(db.DateTime)
