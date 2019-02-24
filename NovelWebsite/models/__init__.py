b63fcf27a1dc926df49988d6a8ecaed9faaf927c
Karloar <879865529@qq.com>
2019-02-24 18:39:13 +0800

    update table

--- NovelWebsite/models/__init__.py
+++ NovelWebsite/models/__init__.py
@@ -7,18 +7,34 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    password = db.Column(db.String(30))
+
+
+class UserCollection(db.Model):
+    __tablename__ = 'user_collection'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
 
 
 class NovelTitle(db.Model):
     __tablename__ = 'novel_title'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    author = db.Column(db.String(50))
 
 
 class NovelSection(db.Model):
     __tablename__ = 'novel_section'
     id = db.Column(db.Integer, primary_key=True)
+    novel_id = db.Column(db.Integer, db.ForeignKey("novel_title.id"))
+    title = db.Column(db.String(200))
+    content = db.Column(db.Text)
+    time = db.Column(db.DateTime)
 
 
 class CrawlSettings(db.Model):
     __tablename__ = 'crawl_settings'
     id = db.Column(db.Integer, primary_key=True)
+    crawl_time = db.Column(db.DateTime)
