c7a76a98fe2552624aebda6de59ae72ab72594af
Karloar <879865529@qq.com>
2019-03-06 17:13:51 +0800

    finish the second page

---
+++ NovelWebsite/views/__init__.py
@@ -0,0 +1,29 @@
+from flask import abort
+from functools import wraps
+
+
+def error_processing(process_func):
+    '''
+    包裹视图函数, 若出现异常直接返回404信息
+    :param process_func:
+    :return:
+    '''
+    @wraps(process_func)
+    def inner_function(*args, **kwargs):
+        try:
+            value = process_func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+            abort(404)
+        return value
+    return inner_function
+
+# def get_page_label_range(current_page, total_page, page_r=3):
+#     page_label_num = 2 * page_r + 1
+#     if total_page <= page_label_num:
+#         return range(1, total_page+1)
+#     if current_page <= page_r:
+#         return range(1, page_label_num+1)
+#     if current_page + page_r >= total_page:
+#         return range(total_page - page_label_num+1, total_page+1)
+#     return range(current_page-page_r, current_page+page_r+1)
--- NovelWebsite/views/index_view.py
+++ NovelWebsite/views/index_view.py
@@ -4,12 +4,14 @@ from ..models import db
 from ..models import NovelType
 from ..models import NovelTitle
 from ..models import NovelSection
+from . import error_processing
 
 
 index_view = Blueprint(__name__, 'index_view')
 
 
 @index_view.route("/", methods=["GET"])
+@error_processing
 def index():
     data = dict()
     data['type_list'] = db.session.query(NovelType).order_by(NovelType.id)
@@ -45,22 +47,83 @@ def index():
         NovelTitle.name.label('title_name'),
         NovelTitle.author,
         NovelType.name.label('type_name')
-    ).join(NovelType).order_by(NovelTitle.id).limit(20)
+    ).join(NovelType).order_by(NovelTitle.id.desc()).limit(20)
+    data['index'] = True
     db.session.remove()
     return render_template("index.html", data=data)
 
 
 @index_view.route("/category/<int:type_id>", methods=['GET'])
-def category(type_id):
+@index_view.route("/category/<int:type_id>/<int:page>", methods=['GET'])
+@error_processing
+def category(type_id, page=1):
     data = dict()
+    per_page = 25
+    data['current_page'] = page
+    data['total'] = db.session.query(NovelSection.id).join(NovelTitle).filter(NovelTitle.type_id == type_id).count()
+    data['page_num'] = data['total'] // per_page if data['total'] % per_page == 0 else data['total'] // per_page + 1
+    if page > data['page_num']:
+        raise Exception('over max pages')
     data['type_list'] = db.session.query(NovelType).order_by(NovelType.id)
     data['type_id'] = type_id
     data['type_name'] = db.session.query(NovelType).filter(NovelType.id == type_id).one().name
+    data['top_novels'] = db.session.query(
+        NovelTitle.id,
+        NovelTitle.name,
+        NovelTitle.author,
+        NovelTitle.cover,
+        NovelTitle.introduction
+    ).filter(NovelTitle.type_id == type_id).order_by(NovelTitle.read_num.desc(), NovelTitle.id.asc()).limit(6)
+    data['last_update_novel_list'] = db.session.query(
+        NovelTitle.id.label('title_id'),
+        NovelTitle.name.label('title_name'),
+        NovelTitle.author,
+        NovelType.name.label('type_name'),
+        NovelSection.title.label("section_name")
+    ).join(NovelType, NovelSection).filter(NovelTitle.type_id == type_id).order_by(
+        NovelSection.id.desc()
+    ).offset(per_page * (page - 1)).limit(per_page)
+    data['recommend_novel_list'] = db.session.query(
+        NovelTitle.id.label('title_id'),
+        NovelTitle.name.label('title_name'),
+        NovelTitle.author,
+        NovelType.name.label('type_name')
+    ).join(NovelType).filter(NovelTitle.type_id == type_id).order_by(
+        NovelTitle.read_num.desc(),
+        NovelTitle.id.asc()
+    ).limit(per_page + 2)
     db.session.remove()
     return render_template("category.html", data=data)
 
 
-@index_view.route("/hehe/<int:novel_id>")
 @index_view.route("/novel/<int:novel_id>", methods=['GET'])
+@error_processing
 def novel(novel_id):
-    return str(novel_id)
+    data = dict()
+    data['type_list'] = db.session.query(NovelType).order_by(NovelType.id)
+    data['novel'] = db.session.query(
+        NovelTitle.id.label('title_id'),
+        NovelTitle.name.label('title_name'),
+        NovelTitle.author,
+        NovelTitle.type_id,
+        NovelTitle.introduction,
+        NovelTitle.cover,
+        NovelType.name.label('type_name')
+    ).join(NovelType).filter(NovelTitle.id == novel_id).one()
+    data['novel_sections'] = db.session.query(
+        NovelSection
+    ).filter(NovelSection.novel_id == novel_id).order_by(NovelSection.novel_id.asc())
+    data['recommend_novel_list'] = db.session.query(NovelTitle).filter(
+        NovelTitle.type_id == data['novel'].type_id
+    ).order_by(NovelTitle.read_num.desc(), NovelTitle.id.asc()).limit(5)
+    data['novel_sections_new'] = db.session.query(
+        NovelSection
+    ).filter(NovelSection.novel_id == novel_id).order_by(NovelSection.id.desc()).limit(6)
+    db.session.remove()
+    return render_template("novel.html", data=data)
+
+
+@index_view.route("/detail/<int:section_id>", methods=['GET'])
+@error_processing
+def detail(section_id):
+    return str(section_id)
