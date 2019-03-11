c23deb413f8fb8c5f70cb3b9fcbc6d34f0c5e691
Karloar <879865529@qq.com>
2019-03-11 16:36:18 +0800

    finish register and modify the user table

--- NovelWebsite/models/__init__.py
+++ NovelWebsite/models/__init__.py
@@ -8,7 +8,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    password = db.Column(db.String(30))
+    password = db.Column(db.String(100))
     email = db.Column(db.String(100))
     user_collections = db.relationship("UserCollection", backref="user", lazy='dynamic')
 
--- NovelWebsite/views/user_view.py
+++ NovelWebsite/views/user_view.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import render_template
 from flask import session
 from . import md5
+from ..models import db
+from ..models import User
 
 
 user_view = Blueprint(__name__, 'user_view')
@@ -10,4 +12,19 @@ user_view = Blueprint(__name__, 'user_view')
 
 @user_view.route("/user/register", methods=['POST'])
 def register():
-    pass
+    user_name = request.form.get('user_name', None)
+    password = request.form.get('password', None)
+    email = request.form.get('email', None)
+    if not user_name or not password or not email:
+        return 'fail'
+    real_password = md5(password)
+    try:
+        db.session.query(User).filter(User.name == user_name, User.password == real_password).one()
+    except Exception as e:
+        print(e)
+        db.session.add(User(name=user_name, password=real_password, email=email))
+        db.session.commit()
+        return 'success'
+    finally:
+        db.session.remove()
+    return 'fail'
